9dac9d589519d9ee3734d9dea9a6067fe4695161
timothyliutl <18tfl1@queensu.ca>
2022-07-25 01:26:23 -0400

    test animation

---
+++ manim-projects.py
@@ -0,0 +1,65 @@
+from cmath import sin
+from tkinter import CENTER
+from typing_extensions import runtime
+from manim import *
+from matplotlib import mathtext
+
+class Riemann_Sum(Scene):
+    def construct(self):
+        plane = NumberPlane(
+           x_range=(-3,3.2,1),
+           y_range=(-1.5,1.5,0.25),
+           x_length=8,
+           y_length=8
+        ).to_edge(DOWN)
+        
+        function = plane.plot(lambda x: sin(x), color=GREEN)
+        rect = plane.get_riemann_rectangles(graph=function, x_range=[0,3,0.2], color=BLUE, stroke_color=GREEN, dx=0.4, fill_opacity=0.5)
+        rect2 = plane.get_riemann_rectangles(graph=function, x_range=[0,3,0.2], color=BLUE, stroke_color=GREEN, dx=0.2, fill_opacity=0.5)
+        rect3 = plane.get_riemann_rectangles(graph=function, x_range=[0,3,0.2], color=BLUE, stroke_color=GREEN, dx=0.1, fill_opacity=0.5)
+        label = MathTex('y = sin(x)')
+        dx_label1 = MathTex('dx = 0.4')
+        dx_label2 = MathTex('dx = 0.2')
+        dx_label3 = MathTex('dx = 0.1')
+
+        dx_label1.next_to(rect)
+        dx_label2.next_to(rect2)
+        dx_label3.next_to(rect3)
+
+        label.set_color(WHITE)
+        label.next_to(function, buff=0.3)
+        self.play(DrawBorderThenFill(plane))
+        self.play(Create(function), runtime=3)
+        self.play(Write(label))
+        self.play(DrawBorderThenFill(rect))
+        self.play(Write(dx_label1))
+        self.wait(3)
+        self.play(FadeTransform(rect, rect2, stretch=True), FadeTransform(dx_label1, dx_label2))
+        self.wait(2)
+        self.play(FadeTransform(rect2, rect3, stretch=True), FadeTransform(dx_label2, dx_label3))
+        self.wait(3)
+
+class Derivative_Definition(Scene):
+    def construct(self):
+        title_text = Text('Definition of a Derivative')
+        self.play(Write(title_text))
+        self.wait(3)
+        self.play(Unwrite(title_text, reverse=False))
+        self.wait(1)
+        limit_def = MathTex(r"f'(x) = \lim_{a \to 0}")
+        limit_def_part2 = MathTex(r"\frac{f(x+a) - f(x)}{a}")
+        limit_def_part2.next_to(limit_def)
+        orig_group = VGroup(limit_def, limit_def_part2)
+        orig_group.arrange(center=True)
+        self.play(Write(orig_group))
+        self.wait(2)
+        # Talk about how this may be a bit daunting and connect it back to the definition of slope
+        limit_def2 = MathTex(r"f'(x) = \lim_{a \to 0} ")
+        limit_def2_part2 = MathTex(r"{", r"f", r"(x+a)", r" - ", r"f", r"(x)", r"}", r"\over", r"{", r"(x+a)", r" - ", r"(x)", r"}" , tex_to_color_map={"(x+a)": BLUE, "(x)": GREEN})
+        limit_def2_part2.next_to(limit_def2)
+        group = VGroup(limit_def2, limit_def2_part2)
+        group.arrange(center=True)
+        self.play(Transform(orig_group, group))
+        self.wait(1)
+
+
